57192e016515015de67f68a53960557efd50eb77
bing1100 <bingxuhu@hotmail.com>
2014-10-08 13:16:14 -0400

    CorrectAnswers
    for p4 and p5

--- p4.py
+++ p4.py
@@ -12,7 +12,7 @@ def f():
             for j in range(998):
                 secondnum = 999-j
                 if (firstnum*secondnum<large): #shifts the lower boundary and optimizes search
-                    large-=j
+                    large-=999
                     break
                 if (ispalindrome(firstnum*secondnum)==1):
                     print(firstnum)
@@ -26,18 +26,23 @@ def f():
 def ispalindrome(num):
     nlength=int(math.floor(math.log10(num)))
     
-    # This is nessesary due to the fact range(0) is empty and for loop closes
-    if (nlength<2):
-        halfway = 1
-    else: 
-        halfway = nlength//2 + 1
-        
+    
+    if (nlength%2==0):
+        halfway=nlength//2
+    else:
+        halfway=nlength//2+1
         
     for i in range(halfway):
+        
+        
+        
         biggerdigit=nlength-i # Is the symmetric larger digit placement
-        print( num%(10**(i+1)), "=", num//(10**biggerdigit))
+                
         # Tests for non palindromes for each i moving closer to halfway
-        if (num%(10**(i+1)) != num//(10**biggerdigit)):
+        if ((num%(10**(i+1))//(10**i)) != num//(10**biggerdigit)):
             return 0
+        
+        num-= num//(10**biggerdigit) * (10**biggerdigit)
+        
         
     return 1 # if number is not a non palindrome than produce 
---
+++ p5.py
@@ -0,0 +1,28 @@
+
+# Run f(topnum) where topnum is the max number in the range
+#  this will produce the LCM of all the numbers in the range
+
+def f(topnum):
+    listofmultiply=[1]
+    multi=1
+    
+    # Goes through the whole list and filtering out GCD
+    for potential in range(1,topnum):
+        
+        #Delete the GCD values from the potential number
+        for divisor in listofmultiply:
+            if (potential%divisor == 0):
+                potential=potential//divisor
+                
+        #Append the potential to the list of multiply         
+        listofmultiply.append(potential)
+        
+    # Computes the final answer
+    for j in listofmultiply:
+        multi*=j
+        
+    return multi
+        
+                
+                
+        
