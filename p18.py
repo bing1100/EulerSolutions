1b0fe790ca3aec09071e5a77c52118302a5b4e0f
bing1100 <bingxuhu@hotmail.com>
2015-01-13 16:26:57 -0500

    p18

---
+++ p18.py
@@ -0,0 +1,70 @@
+#Problem 18 
+
+#By starting at the top of the triangle below and moving to adjacent numbers on the row below, the maximum total from top to bottom is 23.
+
+#3
+#7 4
+#2 4 6
+#8 5 9 3
+
+#That is, 3 + 7 + 4 + 9 = 23.
+
+#Find the maximum total from top to bottom of the triangle below:
+
+#75
+#95 64
+#17 47 82
+#18 35 87 10
+#20 04 82 47 65
+#19 01 23 75 03 34
+#88 02 77 73 07 63 67
+#99 65 04 28 06 16 70 92
+#41 41 26 56 83 40 80 70 33
+#41 48 72 33 47 32 37 16 94 29
+#53 71 44 65 25 43 91 52 97 51 14
+#70 11 33 28 77 73 17 78 39 68 17 57
+#91 71 52 38 17 14 91 43 58 50 27 29 48
+#63 66 04 68 89 53 67 30 73 16 69 87 40 31
+#04 62 98 27 23 09 70 98 73 93 38 53 60 04 23
+
+#NOTE: As there are only 16384 routes, it is possible to solve this problem by trying every route. However, Problem 67, is the same challenge with a triangle containing one-hundred rows; it cannot be solved by brute force, and requires a clever method! ;o)
+
+# Simply use row addition to calculate each whole row, we do not need to identify any paths. 
+
+t_list = [
+75,
+95, 64,
+17, 47, 82,
+18, 35, 87, 10,
+20, 4, 82, 47, 65,
+19, 1, 23, 75, 3, 34,
+88, 2, 77, 73, 7, 63, 67,
+99, 65, 4, 28, 6, 16, 70, 92,
+41, 41, 26, 56, 83, 40, 80, 70, 33,
+41, 48, 72, 33, 47, 32, 37, 16, 94, 29,
+53, 71, 44, 65, 25, 43, 91, 52, 97, 51, 14,
+70, 11, 33, 28, 77, 73, 17, 78, 39, 68, 17, 57,
+91, 71, 52, 38, 17, 14, 91, 43, 58, 50, 27, 29, 48,
+63, 66, 4, 68, 89, 53, 67, 30, 73, 16, 69, 87, 40, 31,
+4, 62, 98, 27, 23, 9, 70, 98, 73, 93, 38, 53, 60, 4, 23]
+
+def first_edge(n):
+    first_edge = n*(n+1)//2
+    return first_edge
+
+
+# Bottom up approach where the levels are combined from the bottom up with the 
+# max sum
+def calculator(py_list,n):
+    temp_list=py_list
+    n_minus=-1
+    
+    # update the n-1 row and by adding the nth row and only using the largest
+    # sum    
+    for row in range(n_minus):
+        c_row=n_minus-row
+        for i in range(n_minus):
+            place = first_edge(c_row)+i
+            temp_list[place] = max(t_list[place]+t_list[n_minus+1],
+                                t_list[place]+t_list[n_minus+2])
+    return t_list[0] 
