48ef9e7081c9cb8375b4a81d00031207d25d0ee6
bing1100 <bingxuhu@hotmail.com>
2017-10-23 14:37:17 -0400

    Completed Decryption solution and found hidden message

--- p59.py
+++ p59.py
@@ -7,7 +7,7 @@ data = [int(x) for x in cipherFile.read().split(",")]
 
 commonStrings = [['t','h','e'], ['a','n','d']]
 
-threshold = 8
+threshold = 10
 
 allDecryptions = []
 prunedDecryptions = []
@@ -43,12 +43,17 @@ def countCommonOcc(datum):
             compareChar = commonStrings[idx][posTrack[idx]]
             if compareChar == char:
                 posTrack[idx] += 1
-            if posTrack[idx] == lenCommonString[idx]:
+                if posTrack[idx] == lenCommonString[idx]:
+                    posTrack[idx] = 0
+                    occTrack[idx] += 1
+            else:
                 posTrack[idx] = 0
-                occTrack[idx] += 1
-    val = reduce(lambda sum,y: sum + y, occTrack)
-    print("Found with " + str(val))
-    return val
+    val = sum(x for x in occTrack)
+    if (val > 0):
+        print("Found with " + str(val))
+    return int(val)
+
+print(countCommonOcc(['t','h','e']))
 
 def pruneDecryptions():
     global allDecryptions
@@ -57,17 +62,28 @@ def pruneDecryptions():
 
     print("Starting to prune")
 
-    map(lambda datum: countCommonOcc(datum), prunedDecryptions)
+    occMap = list(map(lambda datum: countCommonOcc(datum), prunedDecryptions))
+
+    print(occMap)
 
     print("Done Prunning")
 
-    map(lambda num: num >= threshold, prunedDecryptions)
+    printMap = list(map(lambda num: num >= threshold, occMap))
 
-    idx = 0
-    for b in prunedDecryptions:
+    print(printMap)
+
+    idx = 0 
+    for b in printMap:
         if b:
             print("".join(allDecryptions[idx]))
         idx += 1
 
 decrypt()
 pruneDecryptions()
+
+"""
+FOUND THIS IN THE DATA
+
+(The Gospel of John, chapter 1) 1 In the beginning the Word already existed. He was with God, and he was God. 2 He was in the beginning with God. 3 He created everything there is. Nothing exists that he didn't make. 4 Life itself was in him, and this life gives light to everyone. 5 The light shines through the darkness, and the darkness can never extinguish it. 6 God sent John the Baptist 7 to tell everyone about the light so that everyone might believe because of his testimony. 8 John himself was not the light; he was only a witness to the light. 9 The one who is the true light, who gives light to everyone, was going to come into the world. 10 But although the world was made through him, the world didn't recognize him when he came. 11 Even in his own land and among his own people, he was not accepted. 12 But to all who believed him and accepted him, he gave the right to become children of God. 13 They are reborn! This is not a physical birth resulting from human passion or plan, this rebirth comes from God.14 So the Word became human and lived here on earth among us. He was full of unfailing love and faithfulness. And we have seen his glory, the glory of the only Son of the Father.
+
+"""
